d30fece6974b3ad2c22bb5466a5faf2178328ce6
Timo Gatsonides <timo@gatsonides.me>
2026-03-21 00:25:46 +0100

    nd6: backport RA lladdr option check (upstream 7807f706, 6f700a15)

--- src/core/ipv6/nd6.c
+++ src/core/ipv6/nd6.c
@@ -688,7 +688,7 @@ nd6_input(struct pbuf *p, struct ip_globals *ip_data)
       case ND6_OPTION_TYPE_SOURCE_LLADDR:
       {
         struct lladdr_option *lladdr_opt;
-        if (option_len < sizeof(struct lladdr_option)) {
+        if (option_len < (ND6_LLADDR_OPTION_MIN_LENGTH + inp->hwaddr_len)) {
           goto lenerr_drop_free_return;
         }
         lladdr_opt = (struct lladdr_option *)buffer;
